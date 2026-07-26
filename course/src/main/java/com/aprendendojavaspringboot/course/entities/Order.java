181e0afdaf101fb50d48f73ece5d130ab4bb6873
FlavioAlexandreRibeiro <flaviodevfullstack@gmail.com>
2026-07-26 19:10:28 -0300

    OrderStatus enum

--- course/src/main/java/com/aprendendojavaspringboot/course/entities/Order.java
+++ course/src/main/java/com/aprendendojavaspringboot/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.aprendendojavaspringboot.course.entities;
 
+import com.aprendendojavaspringboot.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -19,6 +20,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -28,9 +31,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -42,6 +46,22 @@ public class Order implements Serializable {
         this.id = id;
     }
 
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
+    }
+
     public User getClient() {
         return client;
     }
@@ -50,13 +70,7 @@ public class Order implements Serializable {
         this.client = client;
     }
 
-    public Instant getMoment() {
-        return moment;
-    }
 
-    public void setMoment(Instant moment) {
-        this.moment = moment;
-    }
 
     @Override
     public boolean equals(Object o) {
