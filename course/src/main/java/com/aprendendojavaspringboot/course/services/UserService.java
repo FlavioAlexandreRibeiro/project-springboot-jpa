efafcd2b2bb1dd0496e03ebf9e786a546abd99ea
FlavioAlexandreRibeiro <flaviodevfullstack@gmail.com>
2026-07-26 17:03:56 -0300

    Service layer, component registration

---
+++ course/src/main/java/com/aprendendojavaspringboot/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.aprendendojavaspringboot.course.services;
+
+import com.aprendendojavaspringboot.course.entities.User;
+import com.aprendendojavaspringboot.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findall() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
