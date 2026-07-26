29e08cdc05423754a7fb1e7558cd5b9ce90488c3
FlavioAlexandreRibeiro <flaviodevfullstack@gmail.com>
2026-07-26 18:27:39 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ course/src/main/java/com/aprendendojavaspringboot/course/repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.aprendendojavaspringboot.course.repositories;
+
+import com.aprendendojavaspringboot.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository  extends JpaRepository<Order, Long> {
+}
